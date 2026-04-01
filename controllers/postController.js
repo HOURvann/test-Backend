b6fedd10d38ae3ce60582ba14327ea719eb5c206
vornvannda <vornvannda12@gmail.com>
2026-04-01 14:54:39 +0700

    new

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,23 @@
-const Post = require('../models/Post');
 
 exports.getAllPosts = async (req, res) => {
     const allPosts = await Post.find();
     res.json(allPosts);
 };
-// ... ថែមមុខងារ Create, Update, Delete ផ្សេងៗទៀត
+const Post = require('../models/Post');
+
+// មុខងារទាញយកអត្ថបទតែមួយតាម ID
+exports.getPostById = async (req, res) => {
+    try {
+        // ប្រើ findById ដើម្បីស្វែងរកក្នុង MongoDB
+        const post = await Post.findById(req.params.id);
+        
+        if (!post) {
+            return res.status(404).json({ message: "រកមិនឃើញអត្ថបទនេះទេ!" });
+        }
+        
+        res.json(post);
+    } catch (error) {
+        // បើ ID ខុសទម្រង់ វានឹងលោតមក catch នេះ
+        res.status(500).json({ message: "លេខ ID មិនត្រឹមត្រូវ ឬមានបញ្ហាម៉ាស៊ីនបម្រើ!" });
+    }
+};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,5 +2,10 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
-router.get('/', postController.getAllPosts); // ផ្លូវពេញគឺ /api/v1/posts
+// ផ្លូវសម្រាប់ទាញយកអត្ថបទទាំងអស់
+router.get('/', postController.getAllPosts); 
+
+// ផ្លូវសម្រាប់ទាញយកអត្ថបទតែមួយតាម ID (បន្ថែមជួរនេះ)
+router.get('/:id', postController.getPostById); 
+
 module.exports = router;
